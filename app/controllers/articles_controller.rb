f2134e7988aad0175387aa15eec73ee35b1c1615
darc window <darc.windowwww@gmail.com>
2017-01-01 00:37:07 -0500

    create edit and show page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
 
-# before_Action 
+before_Action :find_article, :only [:show, :edit, :update, :destroy]
+
 def index
 
 end
@@ -10,9 +11,34 @@ def new
 end
 
 def create
-	@article = Article.new(artocle_params)
+	@article = Article.new(article_params)
+
+	if @article.save
+		redirect_to @article, notice: "successfully created article"
+
+	else
+		render 'new'
+	end
+
 end
 
+def edit
+	
+end
+
+
+def update
+	if @article.update(article_params)
+		redirect_to @article, notice: " successfully updated"
+	else
+		render 'edit'
+	end
+end
+
+def destroy
+	@article.destroy
+	redirect_to root_path
+end
 private
 
 def article_params
@@ -20,5 +46,8 @@ def article_params
 	
 end
 
+def find_article
+	@article = Article.find(params[:id])	
+end
 
 end
